6572a6738dcccc3bc47a4285b9dc8f63f435bbf5
pasangtemba <pasangtemba.sherpa5@gmail.com>
2023-07-28 09:08:16 +0545

    Added moer option

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@ namespace MyApp
             Console.WriteLine("Press s for substraction");
             Console.WriteLine("Press m for multiplication");
             Console.WriteLine("Press d for division");
-
+            Console.WriteLine("press 1 to find the area ");
+            Console.WriteLine("press 2 to find the volume");
+            int choice = Convert.ToInt32(Console.ReadLine());
             option = Console.ReadLine();
             switch(option)
             {
@@ -46,6 +48,75 @@ namespace MyApp
                     break;
                     default:
                     Console.WriteLine("Invalid option");
+                    break;
+
+            }
+            switch(choice)
+            {
+                case 1:
+                    Console.WriteLine("press 1 to find the area of rectangle ");
+                    Console.WriteLine("press 2 to find the area of square");
+                    Console.WriteLine("press 3 to find the area of circle");
+                    int choice1 = Convert.ToInt32(Console.ReadLine());
+                    if (choice1==1)
+                    {
+                        Console.WriteLine("enter the length ");
+                        int length = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter the breadth");
+                        int breadth = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("the area of rectangle is " + (length * breadth));
+                    }
+                    else if(choice1== 2)
+                    {
+                        Console.WriteLine("enter the length of square");
+                        int length = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("the area of square is " + (length * length));
+                    }
+                    else if(choice1==3)
+                    {
+                        Console.WriteLine("enter the radius of circle");
+                        int radius = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("the area of circle is " + (3.14 * radius * radius));
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid option");
+                    } 
+                    break;
+                    case 2:
+                    Console.WriteLine("press 1 to find the volume of cube");
+                    Console.WriteLine("press 2 to find the volume of cuboid");
+                    Console.WriteLine("press 3 to find the volume of sphere");
+                    int choice2 = Convert.ToInt32(Console.ReadLine());
+                    if (choice2 == 1) 
+                    {
+                        Console.WriteLine("enter the side of cube");
+                        int side = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("the volume of cube is " + (side * side * side));
+                    }
+                    else if(choice2 == 2)
+                    {
+                          Console.WriteLine("enter the length of cuboid");
+                        int length = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter the breadth of cuboid");
+                        int breadth = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("enter the height of cuboid");
+                        int height = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("the volume of cuboid is " + (length * breadth * height));
+                    }
+                    else if(choice2 == 3)
+                    {
+                        Console.WriteLine("enter the radius of sphere");
+                        int radius = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("the volume of sphere is " + (4/3 * 3.14 * radius * radius * radius));
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid option");    
+                    }
+                    break;
+                    default:
+                    Console.WriteLine("invalid option");
                     break;
             }
                 
